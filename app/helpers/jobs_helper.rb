e79a8969cfab8d6ba9785ab16d7b1b4913c7b358
joshuagou <joshuagou@mail.com>
2017-01-18 01:25:49 +0800

    welcome index changed

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -7,4 +7,23 @@ module JobsHelper
       content_tag(:span, "", :class => "fa fa-globe")
     end
   end
+
+  def job_coll_edit(job)
+      content_tag(:span, "", :class => "fa fa-pencil")
+  end
+
+  def job_coll_Destroy(job)
+      content_tag(:span, "", :class => "fa fa-trash-o")
+  end
+
+  def job_coll_publish(job)
+      content_tag(:span, "", :class => "fa fa-eye")
+  end
+
+  def job_coll_Hide(job)
+      content_tag(:span, "", :class => "fa fa-eye-slash")
+  end
+
+
+
 end
